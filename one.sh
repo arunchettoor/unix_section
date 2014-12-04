337ae50f31d53ef7ea2a40a2924e9f9c8371536f
Arun Chettoor <arunchettoor@users.noreply.github.com>
2014-12-04 23:02:10 +0530

    Update one.sh

--- one.sh
+++ one.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+# New alias command for getting back easily from directory which replaces cd ..
+# Please read this article for more details - http://www.chettoor.in/2014/11/bash-multiple-cd-using-alias.html
+
 _backfunc(){
 	count=$1
 	while [ "$count" -ne 0 ] 
